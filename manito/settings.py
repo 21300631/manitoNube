074a7145563b50d9cf28a10c0e63ae8283838747
Lara Alvarez <a21300631@ceti.mx>
2025-06-18 13:39:25 -0600

    no entendi que cambie pero bueno

--- manito/settings.py
+++ manito/settings.py
@@ -12,35 +12,36 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from dotenv import load_dotenv  # Si usas python-dotenv
 from pathlib import Path
-import os
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+ # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv()
 
-STATIC_URL = '/static/'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 SESSION_COOKIE_AGE = 1209600  # 2 semanas en segundos
 SESSION_EXPIRE_AT_BROWSER_CLOSE = False
-SESSION_SAVE_EVERY_REQUEST = TrueSESSION_COOKIE_AGE = 1209600  # 2 semanas (valor recomendado)
-
-# La sesión expira cuando el usuario cierra el navegador
-SESSION_EXPIRE_AT_BROWSER_CLOSE = False
-
-# Renovar la sesión con cada request
 SESSION_SAVE_EVERY_REQUEST = True
+
 
 LOGIN_URL = '/login/'  # URL donde está el login
 LOGIN_REDIRECT_URL = 'inicio_global'  # Nombre de la URL a donde redirigir tras login
 LOGOUT_REDIRECT_URL = '/login/'  # Para redirigir después de logout
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g3mr!rkd9s-9$(bstwm^5@r-^yq1g)=$jr*_hqwl3k(3r+(a7y'
+SECRET_KEY = os.environ.get('SECRET_KEY', default="$jr*_hqwl3k(3r+(a7y")
+#'django-insecure-g3mr!rkd9s-9$(bstwm^5@r-^yq1g)=$jr*_hqwl3k(3r+(a7y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
@@ -72,6 +73,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     
     'django.middleware.common.CommonMiddleware',
@@ -107,19 +109,14 @@ WSGI_APPLICATION = 'manito.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',        
-        'NAME': 'manito_db',  # Cambia esto por el nombre de tu base de datos
-        'USER': 'postgres',  # Tu usuario de PostgreSQL 
-        'PASSWORD': '140506',  # Tu contraseña de PostgreSQL
-        'HOST': 'localhost',  # Servidor local
-        'PORT': '5432',  # Puerto por defecto de PostgreSQL
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),  # Usa la variable de Render
+        conn_max_age=600  # Conexiones persistentes
+    )
 }
-
 
 
 # Password validation
@@ -156,19 +153,25 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+STATIC_URL = '/static/'
 
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Configurar archivos estáticos y plantillas
 
 # settings.py
 MEDIA_URL = '/media/'  # URL pública para medios
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Ruta absoluta al directorio media/
-STATIC_URL = 'static/'
+
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-load_dotenv()
 
 # Credenciales comunes AWS
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
